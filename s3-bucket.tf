b836f285919016ec70f1dfba1491de41604512a7
nikita199220 <nikita199220@gmail.com>
2021-07-16 12:16:37 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/NIKITA-training/s3-bucket/aws"
   version = "2.2.0"
-  bucket_prefix = "nikita sharma"
-  
+  bucket_prefix = "nikitasharma" 
   # insert required variables here
 }
